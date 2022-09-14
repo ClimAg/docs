f63721c1115fa4f45f3fdbf1c2d6888e46a212fd
Nithiya Streethran <nmstreethran@gmail.com>
2022-09-14 14:48:02 +0100

    update grasscheck plots

--- grass-growth/grasscheck.py
+++ grass-growth/grasscheck.py
@@ -1,3 +1,8 @@
+# %% [markdown]
+# # GrassCheck NI
+#
+# <https://agrisearch.org/grasscheck>
+
 # %%
 import os
 from datetime import datetime, timezone
@@ -21,8 +26,67 @@ grass_ni.shape
 grass_ni.head()
 
 # %%
-grass_ni.plot(x="week", figsize=(14, 5), linewidth=1)
-plt.title("Grass growth data from GrassCheck in Northern Ireland")
+grass_ni.tail()
+
+# %%
+# use weekly time series starting on Monday to fill missing rows
+grass_ts = pd.DataFrame(
+    pd.date_range(
+        str(grass_ni["week"][0].year) + "-01-01",
+        str(grass_ni["week"][len(grass_ni) - 1].year) + "-12-31",
+        freq="W-MON"
+    ),
+    columns=["week"]
+)
+
+# %%
+grass_ts = pd.merge(grass_ts, grass_ni, how="outer")
+
+# %%
+# save time series
+grass_ts.to_csv(DATA_DIR, index=False)
+
+# %%
+# set timestamps as the index
+grass_ts.index = grass_ts["week"]
+
+# %%
+grass_ts.drop(columns=["week"], inplace=True)
+
+# %%
+# capitalise county names
+counties = []
+for c in list(grass_ts):
+    counties.append(c.capitalize())
+
+# %%
+grass_ts.columns = counties
+
+# %%
+grass_ts.plot(figsize=(12, 4), linewidth=1)
+plt.title("Grass growth data for Northern Ireland from GrassCheck NI")
 plt.xlabel("Time")
 plt.ylabel("Grass growth (kg DM/ha/d)")
+plt.show()
+
+# %%
+for c in counties:
+    grass_ts[c].plot(figsize=(12, 4), linewidth=1)
+    plt.title(
+        "Grass growth data for Co. " + c +
+        " from GrassCheck Northern Ireland"
+    )
+    plt.xlabel("Time")
+    plt.ylabel("Grass growth (kg DM/ha/d)")
+    plt.show()
+
+# %%
+years = list(grass_ts.index.year.unique())
+for y in years:
+    grass_ts.loc[str(y)].plot(figsize=(12, 4), linewidth=1.25)
+    plt.title(
+        "Grass growth data from GrassCheck in Northern Ireland in " + str(y)
+    )
+    plt.xlabel("Time")
+    plt.ylabel("Grass growth (kg DM/ha/d)")
 plt.show()
