bd642740799ecfe983794ef367753c8269b439ae
Nithiya Streethran <nmstreethran@gmail.com>
2022-09-29 16:01:34 +0100

    update modvege

--- grass-growth/modvege.py
+++ grass-growth/modvege.py
@@ -3,12 +3,12 @@
 #
 # - Jouven, M., Carrère, P. and Baumont, R. (2006). 'Model predicting dynamics
 #   of biomass, structure and digestibility of herbage in managed permanent
-#   pastures. 1. Model description', *Grass and Forage Science*, vol. 61, no. 2,
-#   pp. 112–124. DOI: [10.1111/j.1365-2494.2006.00515.x][Jouven1].
+#   pastures. 1. Model description', *Grass and Forage Science*, vol. 61,
+#   no. 2, pp. 112-124. DOI: [10.1111/j.1365-2494.2006.00515.x][Jouven1].
 # - Jouven, M., Carrère, P. and Baumont, R. (2006). 'Model predicting dynamics
 #   of biomass, structure and digestibility of herbage in managed permanent
 #   pastures. 2. Model evaluation', *Grass and Forage Science*, vol. 61, no. 2,
-#   pp. 125–133. DOI: [10.1111/j.1365-2494.2006.00517.x][Jouven2].
+#   pp. 125-133. DOI: [10.1111/j.1365-2494.2006.00517.x][Jouven2].
 # - Chemin, Y. (2022). 'modvege', Python. [Online]. Available at
 #   <https://github.com/YannChemin/modvege> (Accessed 6 September 2022).
 #
@@ -29,17 +29,17 @@ print("Last updated:", datetime.now(tz=timezone.utc))
 # %%
 DATA_PATH = os.path.join("data", "grass-growth", "modvege")
 
-# define the name of the input params file (using example data for now)
+# define the name of the input params file
 PARAMS_FILE = os.path.join(DATA_PATH, "params.csv")
-# define the name of the input environment file
-WEATHER_FILE = os.path.join(DATA_PATH, "weather.csv")
+# define the name of the input timeseries file
+TS_FILE = os.path.join(DATA_PATH, "timeseries.csv")
 # outputs
 OUT_FILE = os.path.join(DATA_PATH, "output.csv")
 
 # %%
 # run the main function using the example data
 run_modvege(
-    input_params_csv=PARAMS_FILE,
-    input_weather_csv=WEATHER_FILE,
-    out_csv=OUT_FILE
+    input_params_file=PARAMS_FILE,
+    input_timeseries_file=TS_FILE,
+    out_file=OUT_FILE
 )
