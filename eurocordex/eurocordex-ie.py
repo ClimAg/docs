8473aea8616f62c0af39006ee0e9e9f472c844ae
Nithiya Streethran <nmstreethran@gmail.com>
2022-10-05 11:57:44 +0100

    simplify EURO-CORDEX IE script

--- eurocordex/eurocordex-ie.py
+++ eurocordex/eurocordex-ie.py
@@ -10,6 +10,9 @@ import intake
 import matplotlib.pyplot as plt
 import xarray as xr
 import climag.plot_configs as cplt
+import itertools
+import pandas as pd
+from sqlalchemy import create_engine
 
 # %%
 print("Last updated:", datetime.now(tz=timezone.utc))
@@ -31,9 +34,6 @@ LAT = 51.84722
 # Ireland boundary
 GPKG_BOUNDARY = os.path.join("data", "boundary", "boundaries.gpkg")
 ie = gpd.read_file(GPKG_BOUNDARY, layer="OS_IE_Ireland_ITM")
-
-# %%
-MODVEGE_DIR = os.path.join("data", "grass-growth", "modvege")
 
 # %% [markdown]
 # ## Reading the local catalogue
@@ -63,13 +63,13 @@ cordex_eur11_cat.df.shape
 cordex_eur11_cat.df.head()
 
 # %% [markdown]
-# ## Read a subset (precipitation)
+# ## Read a subset (rcp85)
 
 # %%
 # filter data subset
 cordex_eur11 = cordex_eur11_cat.search(
     experiment_id="rcp85",
-    variable_id="pr",
+    variable_id=["pr", "tas", "evspsblpot"],
     institute_id="SMHI"
 )
 
@@ -117,11 +117,59 @@ data = data.rio.clip(ie.envelope.buffer(10000).to_crs(data.rio.crs))
 data
 
 # %%
+data.dims
+
+# %% [markdown]
+# ### Convert units
+
+# %%
+for v in data.data_vars:
+    var_attrs = data[v].attrs  # extract attributes
+    if v == "tas":
+        var_attrs["units"] = "°C"  # convert K to deg C
+        data[v] = data[v] - 273.15
+    else:
+        var_attrs["units"] = "mm/day"  # convert kg m-2 s-1 to mm/day
+        data[v] = data[v] * 60 * 60 * 24
+    data[v].attrs = var_attrs  # reassign attributes
+
+# %%
+data
+
+# %% [markdown]
+# ### Export data
+
+# %%
 # export to NetCDF
-FILE_NAME = cplt.ie_ncfile_name(data)
+FILE_NAME = cplt.ie_cordex_ncfile_name(data)
 
 # %%
 data.to_netcdf(os.path.join(DATA_DIR, FILE_NAME))
+
+# %%
+# len(list(itertools.product(
+#     range(len(data["pr"]["rlon"])), range(len(data["pr"]["rlat"]))
+# ))) == len(data["pr"]["rlon"]) * len(data["pr"]["rlat"])
+
+# %%
+# TS_OUT_DIR = os.path.join(
+#     os.path.split(cordex_eur11.df["uri"][0])[0], "timeseries"
+# )
+# TS_FILE_BASE = os.path.split(cordex_eur11.df["uri"][0])[1][:-20]
+# engine = create_engine("sqlite://", echo=False)
+# os.makedirs(TS_OUT_DIR, exist_ok=True)
+# for x, y in itertools.product(
+#     range(len(data["pr"]["rlon"])), range(len(data["pr"]["rlat"]))
+# ):
+#     data_ie = data.isel(rlon=x, rlat=y)
+#     data_df = pd.DataFrame(
+#         {"time": data_ie["pr"]["time"], "pr": data_ie["pr"]}
+#     )
+#     if data_df["pr"].isna().all() == False:
+#         TS_OUT_FILE = os.path.join(
+#             TS_OUT_DIR, TS_FILE_BASE + str(x) + "-" + str(y) + ".csv"
+#         )
+#         data_df.to_csv(TS_OUT_FILE, index=False)
 
 # %% [markdown]
 # #### Time subset
@@ -133,266 +181,63 @@ data_ie = data.isel(time=50)
 data_ie
 
 # %%
-plot_transform = cplt.rotated_pole_transform(data_ie)
-plot_data = data_ie["pr"] * 60 * 60 * 24  # convert to mm/day
-cbar_label = data_ie["pr"].attrs["long_name"] + " [mm/day]"  # colorbar label
+for v in data.data_vars:
+    cbar_label = (
+        data_ie[v].attrs["long_name"] + " [" + data_ie[v].attrs["units"] + "]"
+    )  # colorbar label
+    if v == "tas":
+        cmap = "Spectral_r"
+    else:
+        cmap = "GnBu"
+    plot_transform = cplt.rotated_pole_transform(data_ie)
 
-plt.figure(figsize=(20, 10))
-ax = plt.axes(projection=plot_transform)
+    plt.figure(figsize=(20, 10))
+    ax = plt.axes(projection=plot_transform)
 
-# specify gridline spacing and labels
-ax.gridlines(
-    draw_labels=True,
-    xlocs=range(-180, 180, 2),
-    ylocs=range(-90, 90, 1),
-    color="lightslategrey",
-    linewidth=.5
-)
+    # specify gridline spacing and labels
+    ax.gridlines(
+        draw_labels=True,
+        xlocs=range(-180, 180, 2),
+        ylocs=range(-90, 90, 1),
+        color="lightslategrey",
+        linewidth=.5
+    )
 
-# plot data for the variable
-plot_data.plot(
-    ax=ax,
-    cmap="GnBu",
-    transform=plot_transform,
-    x="rlon",
-    y="rlat",
-    cbar_kwargs=dict(label=cbar_label)
-)
+    # plot data for the variable
+    data_ie[v].plot(
+        ax=ax,
+        cmap=cmap,
+        transform=plot_transform,
+        x="rlon",
+        y="rlat",
+        cbar_kwargs=dict(label=cbar_label)
+    )
 
-# add boundaries
-ax.coastlines(resolution="10m", color="darkslategrey", linewidth=.75)
+    # add boundaries
+    ax.coastlines(resolution="10m", color="darkslategrey", linewidth=.75)
 
-ax.set_title(cplt.cordex_plot_title(data_ie))  # set plot title
+    ax.set_title(cplt.cordex_plot_title(data_ie))  # set plot title
 
-plt.show()
+    plt.show()
 
 # %% [markdown]
 # #### Point subset
 
 # %%
+# using Cork Airport met station coordinates
 cds = cplt.rotated_pole_point(data=data, lon=LON, lat=LAT)
 
 # %%
 data_ie = data.sel({"rlon": cds[0], "rlat": cds[1]}, method="nearest")
 
 # %%
-data_ie
-
-# %%
-plt.figure(figsize=(12, 4))
-plt.plot(data_ie["time"], data_ie["pr"] * 60 * 60 * 24)
-plt.xlabel(data_ie["time"].attrs["standard_name"])
-plt.ylabel(data_ie["pr"].attrs["long_name"] + " [mm/day]")
-plt.title(cplt.cordex_plot_title(data_ie, lon=LON, lat=LAT))
-plt.tight_layout()
-plt.show()
-
-# %% [markdown]
-# ## Read a subset (evapotranspiration)
-
-# %%
-# filter data subset
-cordex_eur11 = cordex_eur11_cat.search(
-    experiment_id="rcp85",
-    variable_id="evspsblpot",
-    institute_id="SMHI"
-)
-
-# %%
-cordex_eur11
-
-# %%
-cordex_eur11.df
-
-# %%
-data = xr.open_mfdataset(
-    list(cordex_eur11.df["uri"]),
-    chunks="auto",
-    decode_coords="all"
-)
-
-# %%
-data
-
-# %% [markdown]
-# ### Ireland subset
-
-# %%
-# clip to Ireland's bounding box with a 10 km buffer
-data = data.rio.clip(ie.envelope.buffer(10000).to_crs(data.rio.crs))
-
-# %%
-data
-
-# %%
-# export to NetCDF
-FILE_NAME = cplt.ie_ncfile_name(data)
-
-# %%
-data.to_netcdf(os.path.join(DATA_DIR, FILE_NAME))
-
-# %% [markdown]
-# #### Time subset
-
-# %%
-data_ie = data.isel(time=50)
-
-# %%
-data_ie
-
-# %%
-plot_transform = cplt.rotated_pole_transform(data_ie)
-plot_data = data_ie["evspsblpot"] * 60 * 60 * 24  # convert to mm/day
-cbar_label = data_ie["evspsblpot"].attrs["long_name"] + " [mm/day]"
-
-plt.figure(figsize=(20, 10))
-ax = plt.axes(projection=plot_transform)
-
-# specify gridline spacing and labels
-ax.gridlines(
-    draw_labels=True,
-    xlocs=range(-180, 180, 2),
-    ylocs=range(-90, 90, 1),
-    color="lightslategrey",
-    linewidth=.5
-)
-
-# plot data for the variable
-plot_data.plot(
-    ax=ax,
-    cmap="GnBu",
-    transform=plot_transform,
-    x="rlon",
-    y="rlat",
-    cbar_kwargs=dict(label=cbar_label)
-)
-
-# add boundaries
-ax.coastlines(resolution="10m", color="darkslategrey", linewidth=.75)
-
-ax.set_title(cplt.cordex_plot_title(data_ie))  # set plot title
-
-plt.show()
-
-# %% [markdown]
-# #### Point subset
-
-# %%
-cds = cplt.rotated_pole_point(data=data, lon=LON, lat=LAT)
-
-# %%
-data_ie = data.sel({"rlon": cds[0], "rlat": cds[1]}, method="nearest")
-
-# %%
-plt.figure(figsize=(12, 4))
-plt.plot(data_ie["time"], data_ie["evspsblpot"] * 60 * 60 * 24)
-plt.xlabel(data_ie["time"].attrs["standard_name"])
-plt.ylabel(data_ie["evspsblpot"].attrs["long_name"] + " [mm/day]")
-plt.title(cplt.cordex_plot_title(data_ie, lon=LON, lat=LAT))
-plt.tight_layout()
-plt.show()
-
-# %% [markdown]
-# ## Read a subset (temperature)
-
-# %%
-# filter data subset
-cordex_eur11 = cordex_eur11_cat.search(
-    experiment_id="rcp85",
-    variable_id="tas",
-    institute_id="SMHI"
-)
-
-# %%
-cordex_eur11
-
-# %%
-cordex_eur11.df
-
-# %%
-data = xr.open_mfdataset(
-    list(cordex_eur11.df["uri"]),
-    chunks="auto",
-    decode_coords="all"
-)
-
-# %%
-data
-
-# %% [markdown]
-# ### Ireland subset
-
-# %%
-# clip to Ireland's bounding box with a 10 km buffer
-data = data.rio.clip(ie.envelope.buffer(10000).to_crs(data.rio.crs))
-
-# %%
-data
-
-# %%
-# export to NetCDF
-FILE_NAME = cplt.ie_ncfile_name(data)
-
-# %%
-data.to_netcdf(os.path.join(DATA_DIR, FILE_NAME))
-
-# %% [markdown]
-# #### Time subset
-
-# %%
-data_ie = data.isel(time=50)
-
-# %%
-data_ie
-
-# %%
-plot_transform = cplt.rotated_pole_transform(data_ie)
-plot_data = data_ie["tas"] - 273.15  # convert to deg C
-cbar_label = data_ie["tas"].attrs["long_name"] + " [°C]"
-
-plt.figure(figsize=(20, 10))
-ax = plt.axes(projection=plot_transform)
-
-# specify gridline spacing and labels
-ax.gridlines(
-    draw_labels=True,
-    xlocs=range(-180, 180, 2),
-    ylocs=range(-90, 90, 1),
-    color="lightslategrey",
-    linewidth=.5
-)
-
-# plot data for the variable
-plot_data.plot(
-    ax=ax,
-    cmap="Spectral_r",
-    transform=plot_transform,
-    x="rlon",
-    y="rlat",
-    cbar_kwargs=dict(label=cbar_label)
-)
-
-# add boundaries
-ax.coastlines(resolution="10m", color="darkslategrey", linewidth=.75)
-
-ax.set_title(cplt.cordex_plot_title(data_ie))  # set plot title
-
-plt.show()
-
-# %% [markdown]
-# #### Point subset
-
-# %%
-cds = cplt.rotated_pole_point(data=data, lon=LON, lat=LAT)
-
-# %%
-data_ie = data.sel({"rlon": cds[0], "rlat": cds[1]}, method="nearest")
-
-# %%
-plt.figure(figsize=(12, 4))
-plt.plot(data_ie["time"], data_ie["tas"] * 60 * 60 * 24)
-plt.xlabel(data_ie["time"].attrs["standard_name"])
-plt.ylabel(data_ie["tas"].attrs["long_name"] + " [°C]")
-plt.title(cplt.cordex_plot_title(data_ie, lon=LON, lat=LAT))
-plt.tight_layout()
-plt.show()
+for v in data.data_vars:
+    plt.figure(figsize=(12, 4))
+    plt.plot(data_ie["time"], data_ie[v])
+    plt.xlabel(data_ie["time"].attrs["standard_name"])
+    plt.ylabel(
+        data_ie[v].attrs["long_name"] + " [" + data_ie[v].attrs["units"] + "]"
+    )
+    plt.title(cplt.cordex_plot_title(data_ie, lon=LON, lat=LAT))
+    plt.tight_layout()
+    plt.show()
