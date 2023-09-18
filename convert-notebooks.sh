979254f0eb543d04cb4ff849bbb8eeef3e651daf
Nithiya Streethran <nmstreethran@gmail.com>
2023-09-18 14:56:52 +0100

    update fig label

--- convert-notebooks.sh
+++ convert-notebooks.sh
@@ -21,13 +21,3 @@ mkdir ../draft/nb
 # copy files to temporary directory
 # https://unix.stackexchange.com/a/132601
 cp **/*.py --parents ../draft/nb
-
-# git checkout main
-
-# # delete existing Python scripts
-# rm */*.py
-
-# cd ../draft/nb/
-
-# # https://stackoverflow.com/a/63841503
-# cp **/*.py --parents ../../jupyter-notebooks
