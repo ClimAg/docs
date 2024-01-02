3caadaf4f50c1e14531d49f4108464fc53eae62b
Nithiya Streethran <nmstreethran@gmail.com>
2024-01-02 14:31:56 +0000

    update docs configs

--- conf.py
+++ conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+
+sys.path.append('..')
+
 project = 'ClimAg'
 copyright = '2022-2024, Nithiya Streethran'
 author = 'Nithiya Streethran'
@@ -13,15 +17,21 @@ author = 'Nithiya Streethran'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'myst_nb']
+
+# disable sorting of functions by alphabetical order
+autodoc_member_order = 'bysource'
+
+# do not execute Jupyter notebooks
+nb_execution_mode = 'off'
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.ipynb_checkpoints', '.pytest_cache', '__pycache__', '.coverage', 'environment.yml', 'convert-notebooks.sh', '.gitignore', 'README.md', 'LICENCE.txt']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
